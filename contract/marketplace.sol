1af17928cf83d33883b3c62034290fa21eaf1e74
eliashezron <eliashezron23@gmail.com>
2022-08-26 17:38:31 +0300

    ready to deploy

--- contract/marketplace.sol
+++ contract/marketplace.sol
@@ -16,10 +16,11 @@ interface IERC20Token {
 
 contract Marketplace {
 
+    // string internal product;
+    // mapping (uint => string) internal products;
     uint internal productsLength = 0;
     address internal cUsdTokenAddress = 0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1;
-
-    struct Product {
+       struct Product {
         address payable owner;
         string name;
         string image;
@@ -28,62 +29,61 @@ contract Marketplace {
         uint price;
         uint sold;
     }
+     mapping (uint => Product) internal products;
 
-    mapping (uint => Product) internal products;
 
-    function writeProduct(
-        string memory _name,
-        string memory _image,
-        string memory _description, 
-        string memory _location, 
-        uint _price
-    ) public {
-        uint _sold = 0;
-        products[productsLength] = Product(
-            payable(msg.sender),
-            _name,
-            _image,
-            _description,
-            _location,
-            _price,
-            _sold
-        );
+   function writeProduct(
+		string memory _name,
+		string memory _image,
+		string memory _description, 
+		string memory _location, 
+		uint _price
+	) public {
+		uint _sold = 0;
+		products[productsLength] = Product(
+			payable(msg.sender),
+			_name,
+			_image,
+			_description,
+			_location,
+			_price,
+			_sold
+		);
         productsLength++;
-    }
+	}
 
-    function readProduct(uint _index) public view returns (
-        address payable,
-        string memory, 
-        string memory, 
-        string memory, 
-        string memory, 
-        uint, 
-        uint
-    ) {
-        return (
-            products[_index].owner,
-            products[_index].name, 
-            products[_index].image, 
-            products[_index].description, 
-            products[_index].location, 
-            products[_index].price,
-            products[_index].sold
-        );
-    }
-    
-    function buyProduct(uint _index) public payable  {
-        require(
-          IERC20Token(cUsdTokenAddress).transferFrom(
-            msg.sender,
-            products[_index].owner,
-            products[_index].price
-          ),
-          "Transfer failed."
-        );
-        products[_index].sold++;
-    }
-    
-    function getProductsLength() public view returns (uint) {
+   	function readProduct(uint _index) public view returns (
+		address payable,
+		string memory, 
+		string memory, 
+		string memory, 
+		string memory, 
+		uint, 
+		uint
+	) {
+		return (
+			products[_index].owner, 
+			products[_index].name, 
+			products[_index].image, 
+			products[_index].description, 
+			products[_index].location, 
+			products[_index].price,
+			products[_index].sold
+		);
+	}
+    	function buyProduct(uint _index) public payable  {
+		require(
+		  IERC20Token(cUsdTokenAddress).transferFrom(
+			msg.sender,
+			products[_index].owner,
+			products[_index].price
+		  ),
+		  "Transfer failed."
+		);
+		products[_index].sold++;
+	}
+
+        function getProductsLength() public view returns (uint) {
         return (productsLength);
     }
 }
